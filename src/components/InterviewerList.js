39b626acc5548dbc120b6929e8e2f46dc651cb87
Mohamed <justmoe23@gmail.com>
2022-01-21 20:37:22 -0500

    update props and state of form component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,7 +9,7 @@ function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
+        selected={interviewer.id === props.interviewer}
         setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
