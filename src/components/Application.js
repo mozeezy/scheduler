887034c6a423200443923ec8129c2cf5609da125
Mohamed <justmoe23@gmail.com>
2022-01-23 18:15:45 -0500

    install axios and make a GET request to the scheduler api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
@@ -44,23 +45,13 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then((response) => setDays(response.data))
+    .catch((err) => {console.log(err)})
+  },[])
   return (
     <main className="layout">
       <section className="sidebar">
