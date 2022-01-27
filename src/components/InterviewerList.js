6a13431e0247ba6fb7959e96d907a45329aeb1c4
Mohamed <justmoe23@gmail.com>
2022-01-26 21:45:41 -0500

    implement the prop-types library

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 function InterviewerList(props) {
   const interviewers = props.interviewers;
@@ -23,5 +24,8 @@ function InterviewerList(props) {
     </section>
   );
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default InterviewerList;
